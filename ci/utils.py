9a59d21fb5eb17cafa776538857f95a99fc133d3
Aurelien Gateau <mail@agateau.com>
2023-01-21 17:21:27 +0100

    Make ci/fetch use clyde from latest snapshot

---
+++ ci/utils.py
@@ -0,0 +1,16 @@
+import os
+import platform
+import shutil
+import sys
+
+
+IS_WINDOWS = platform.system() == "Windows"
+IS_LINUX = platform.system() == "Linux"
+IS_MACOS = platform.system() == "Darwin"
+
+
+def which(cmd: str) -> str:
+    cmd_path = shutil.which(cmd)
+    if cmd_path is None:
+        sys.exit(f"Can't find {cmd} in {os.environ['PATH']}")
+    return cmd_path
