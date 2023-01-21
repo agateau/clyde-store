bf51ca79794243c4eadce502db99594fbeac5e68
Aurelien Gateau <mail@agateau.com>
2023-01-21 18:03:53 +0100

    ci/fetch: do not download clyde in --only-pr mode

--- ci/tui.py
+++ ci/tui.py
@@ -2,9 +2,11 @@ import sys
 
 
 if sys.stdout.isatty():
+    ORANGE = "\033[33m"
     CYAN = "\033[36m"
     RESET = "\033[0;0m"
 else:
+    ORANGE = ""
     CYAN = ""
     RESET = ""
 
@@ -15,3 +17,7 @@ def eprint(msg: str) -> None:
 
 def progress(msg: str) -> None:
     eprint(f"{CYAN}{msg}{RESET}")
+
+
+def warning(msg: str) -> None:
+    eprint(f"{ORANGE}{msg}{RESET}")
