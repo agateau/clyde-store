6bcbbbe80180cee030a76b82d7456aa117dc51a5
Aurelien Gateau <mail@agateau.com>
2025-03-08 18:16:54 +0100

    Go back to the previous way of running CI
    The new system is clumsy and over-complicated.

--- ci/check_packages.py
+++ ci/check_packages.py
@@ -7,26 +7,21 @@ import random
 import subprocess
 import sys
 from pathlib import Path
-from typing import Iterable, List
+from typing import Iterable
 
-from get_clyde import (  # find_clyde_release_url,
+from get_clyde import (
     GITHUB_TOKEN,
     download_clyde,
+    find_clyde_release_url,
     find_clyde_snapshot_url,
 )
 from git import Repo
 from tui import eprint, progress
-from utils import (
-    CI_DIR,
-    CLYDE_DIR,
-    ROOT_DIR,
-    get_target_branch,
-    is_package,
-    list_all_packages,
-    which,
-)
+from utils import CLYDE_DIR, ROOT_DIR, is_package, list_all_packages, which
 
 RANDOM_COUNT = 8
+
+CI_DIRNAME = "ci"
 
 
 def check_github_token() -> None:
@@ -40,37 +35,39 @@ def check_github_token() -> None:
 def iter_modified_files(repo: Repo, revision: str) -> Iterable[Path]:
     diff_index = repo.index.diff(revision)
     for diff in diff_index.iter_change_type("M"):
+        assert diff.b_path
         yield Path(diff.b_path)
     for diff in diff_index.iter_change_type("A"):
+        assert diff.b_path
         yield Path(diff.b_path)
 
 
-def has_ci_changed(paths: List[Path]) -> bool:
-    return any(CI_DIR in x.parents for x in paths)
+def has_ci_changed(repo: Repo, paths: list[Path]) -> bool:
+    return any(Path(CI_DIRNAME) in x.parents for x in paths)
 
 
-def list_packages_to_check(repo: Repo, revision: str) -> List[Path]:
+def list_packages_to_check(
+    repo: Repo, revision: str, *, random_count: int = RANDOM_COUNT
+) -> list[Path]:
     progress("Listing packages to check")
     paths = list(iter_modified_files(repo, revision))
     packages = [x for x in paths if is_package(x)]
-    if has_ci_changed(paths):
+    if has_ci_changed(repo, paths):
         eprint(
-            f"CI files have changed: adding a random selection of {RANDOM_COUNT}"
+            f"CI files have changed: adding a random selection of {random_count}"
             " packages"
         )
-        all_packages = list_all_packages()
+        all_packages = list_all_packages(repo)
         candidates = list(set(all_packages) - set(packages))
-        if len(candidates) <= RANDOM_COUNT:
+        if len(candidates) <= random_count:
             return all_packages
-        packages.extend(random.sample(candidates, RANDOM_COUNT))
+        packages.extend(random.sample(candidates, random_count))
     return packages
 
 
-def check_packages(packages: List[Path], report_path: str | None) -> None:
+def check_packages(packages: list[Path]) -> None:
     # We must run the test in the current directory for now
     cmd = [which("clydetools"), "check"] + packages
-    if report_path:
-        cmd.extend(["--report", report_path])
     subprocess.run(cmd, cwd=ROOT_DIR)
 
 
@@ -79,6 +76,7 @@ def main() -> int:
         formatter_class=argparse.RawDescriptionHelpFormatter, description=__doc__
     )
 
+    parser.add_argument("target")
     parser.add_argument("-a", "--all", action="store_true")
     parser.add_argument("-d", "--dry-run", action="store_true")
     parser.add_argument(
@@ -86,21 +84,17 @@ def main() -> int:
         metavar="REVISION",
         help="Check packages changed in REVISION..HEAD instead of origin/TARGET..HEAD",
     )
-    parser.add_argument(
-        "-r", "--report", help="Path where to store a clydetools check report"
-    )
 
     args = parser.parse_args()
 
     check_github_token()
 
     repo = Repo(".")
-    target = get_target_branch(repo)
 
     if args.all:
-        packages = list_all_packages()
+        packages = list_all_packages(repo)
     else:
-        revision = args.rev if args.rev else f"origin/{target}"
+        revision = args.rev if args.rev else f"origin/{args.target}"
         packages = list_packages_to_check(repo, revision)
     packages = sorted(packages)
 
@@ -114,16 +108,13 @@ def main() -> int:
     if args.dry_run:
         return 0
 
-    # Disable this because we need the --report option, which is not in 0.8.0.
-    # TODO: uncomment when 0.9.0 is out.
-    # if target == "main":
-    #     clyde_url = find_clyde_release_url()
-    # else:
-    #     clyde_url = find_clyde_snapshot_url()
-    clyde_url = find_clyde_snapshot_url()
+    if args.target == "main":
+        clyde_url = find_clyde_release_url()
+    else:
+        clyde_url = find_clyde_snapshot_url()
 
     download_clyde(clyde_url, CLYDE_DIR)
-    check_packages(packages, args.report)
+    check_packages(packages)
     return 0
 
 
--- ci/fetch.py
+++ ci/fetch.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 """
-Fetches updates for specified packages, push a branch with all modified packages.
+Fetches updates for specified packages using `clydetools fetch`.
 """
 import argparse
 import subprocess
 import sys
-from datetime import datetime
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
 from get_clyde import download_clyde, find_clyde_snapshot_url
 from git import Repo
-from tui import progress
-from utils import GIT_AUTHOR, get_modified_packages, list_all_packages
+from utils import list_all_packages
 
 
 def run_clydetools_fetch(package_files: list[Path]) -> None:
@@ -27,37 +25,9 @@ def run_clydetools_fetch(package_files: list[Path]) -> None:
         )
 
 
-def create_proposed_branch(repo: Repo, *, is_next: bool) -> str:
-    """Create a branch and commit all modified packages to it.
-    Returns the branch name.
-    """
-    base_branch = "next" if is_next else "main"
-    timestamp = datetime.now().isoformat()
-    timestamp_slug = timestamp.replace(":", "")
-    branch_name = f"{base_branch}-proposed-{timestamp_slug}"
-
-    progress(f"Creating proposed branch: '{branch_name}'")
-    branch = repo.create_head(branch_name)
-    branch.checkout()
-    repo.index.add(get_modified_packages(repo))
-    repo.index.commit(
-        f"Proposed updates for {base_branch} - {timestamp}",
-        author=GIT_AUTHOR,
-        committer=GIT_AUTHOR,
-    )
-
-    return branch_name
-
-
 def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter, description=__doc__
-    )
-
-    parser.add_argument(
-        "--next",
-        action="store_true",
-        help="Target 'next' instead of 'main'",
     )
 
     parser.add_argument("package_files", nargs="*")
@@ -67,14 +37,10 @@ def main():
     if args.package_files:
         package_paths = [Path(x) for x in args.package_files]
     else:
-        package_paths = list_all_packages()
+        repo = Repo(".")
+        package_paths = list_all_packages(repo)
 
-    repo = Repo(Path("."))
     run_clydetools_fetch(package_paths)
-    branch_name = create_proposed_branch(repo, is_next=args.next)
-
-    progress("Pushing proposed branch")
-    repo.git.push("-u", "origin", branch_name)
 
     return 0
 
